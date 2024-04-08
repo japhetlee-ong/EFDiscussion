11e5d720f7eada2392918dd20cf548c82f9a308a
Japhet Ong <japhetlee.dev@gmail.com>
2024-04-08 21:38:00 +0800

    Add project files.

---
+++ Controllers/BlogPostsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EFDiscussion.Models;
+
+namespace EFDiscussion.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private readonly BlogsContext _context;
+
+        public BlogPostsController(BlogsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BlogPosts
+        public async Task<IActionResult> Index()
+        {
+            var blogsContext = _context.BlogPosts.Include(b => b.Author);
+            return View(await blogsContext.ToListAsync());
+        }
+
+        // GET: BlogPosts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPosts
+                .Include(b => b.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Create
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id");
+            return View();
+        }
+
+        // POST: BlogPosts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Slug,BlogContent,AuthorId")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogPost);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", blogPost.AuthorId);
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", blogPost.AuthorId);
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Slug,BlogContent,AuthorId")] BlogPost blogPost)
+        {
+            if (id != blogPost.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogPost);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogPostExists(blogPost.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", blogPost.AuthorId);
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPosts
+                .Include(b => b.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blogPost = await _context.BlogPosts.FindAsync(id);
+            if (blogPost != null)
+            {
+                _context.BlogPosts.Remove(blogPost);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return _context.BlogPosts.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/BlogsContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDiscussion.Models;
+
+public partial class BlogsContext : DbContext
+{
+    public BlogsContext()
+    {
+    }
+
+    public BlogsContext(DbContextOptions<BlogsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<BlogPost> BlogPosts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.DateCreated).HasColumnType("datetime");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<BlogPost>(entity =>
+        {
+            entity.ToTable("BlogPost");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.BlogContent).HasColumnType("text");
+            entity.Property(e => e.Slug)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.BlogPosts)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BlogPost_Authors");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
